5096ba0cbea58e77441e275c0db284cc236b3f56
davgame <davshorvoglyan@rambler.ru>
2024-05-27 23:32:23 +0300

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({ registerType: 'autoUpdate' })
+  ],
   resolve:{
     alias:{
       '@':path.resolve(__dirname, './src')
