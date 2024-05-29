4ad524fcaaa757ea47ef10d20576c22872ebadbf
davgame <davshorvoglyan@rambler.ru>
2024-05-29 14:29:52 +0300

    commit

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,8 @@ export default defineConfig({
           {
             "src": "favicon-32x32.png",
             "sizes": "32x32",
-            "type": "image/png"
+            "type": "image/png",
+            "purpose": "maskable"
           },
           {
             "src": "download.png",
@@ -25,22 +26,26 @@ export default defineConfig({
           {
             "src": "apple-touch-icon.png",
             "sizes": "180x180",
-            "type": "image/png"
+            "type": "image/png",
+            "purpose": "maskable"
           },
           {
             "src": "android-chrome.png",
             "sizes": "192x192",
-            "type": "image/png"
+            "type": "image/png",
+            "purpose": "maskable"
           }, 
           {
             "src": "android-chrome-256.png",
             "sizes": "256x256",
-            "type": "image/png"
+            "type": "image/png",
+            "purpose": "maskable"
           },
           {
             "src": "favicon-512-512.png",
             "sizes": "512x512",
-            "type": "image/png"
+            "type": "image/png",
+            "purpose": "maskable"
           }
         ]
       }
