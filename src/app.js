45f98d6f1f871e146d75505d750b47f5533cbe0b
davgame <davshorvoglyan@rambler.ru>
2024-05-31 01:02:30 +0300

    commit

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/dist/sw.js')
+      navigator.serviceWorker.register('sw.js')
         .then(registration => {
           console.log('Зарегестрировали', registration.scope);
         })
