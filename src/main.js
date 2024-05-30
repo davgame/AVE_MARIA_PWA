f996041142502d6a89fae4a6c04cf909ca536c0e
davgame <davshorvoglyan@rambler.ru>
2024-05-30 21:39:32 +0300

    commit

--- src/main.js
+++ src/main.js
@@ -10,15 +10,4 @@ import router from '@/router/router'
 
 createApp(App).use(router).mount('#app')
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('sw.js')
-      .then(registration => {
-        console.log('Зарегестрировали', registration.scope);
-      })
-      .catch(error => {
-        console.log('Ошибка регистрации', error);
-      });
-  });
-}
 
